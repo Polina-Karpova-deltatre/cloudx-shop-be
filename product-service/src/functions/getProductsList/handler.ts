906ecd86f41a099c6985eff9c22c6a754ee1b889
Polina Karpova <Polina.Karpova@deltatre.com>
2023-10-16 21:37:29 +0700

    test-4: Integration with NoSQL Database

--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -2,11 +2,33 @@ import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
 import { formatJSONResponse, formatErrorResponse } from "@libs/api-gateway";
 import { middyfy } from "@libs/lambda";
 
-import { getProducts } from "@functions/data";
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
-const getProductsList: ValidatedEventAPIGatewayProxyEvent<
-  unknown
-> = async () => {
+const getProducts = async () => {
+  const productScanResults = await dynamo
+    .scan({ TableName: process.env.PRODUCTS_TABLE_NAME })
+    .promise();
+  const stockScanResults = await dynamo
+    .scan({ TableName: process.env.STOCKS_TABLE_NAME })
+    .promise();
+
+  return productScanResults.Items.map((item) => {
+    const count = stockScanResults.Items.find(
+      (stock) => stock.product_id === item.id
+    ).count;
+
+    return {
+      ...item,
+      count,
+    };
+  });
+};
+
+const getProductsList: ValidatedEventAPIGatewayProxyEvent<unknown> = async (
+  event
+) => {
+  console.log(event);
   try {
     const products = await getProducts();
     return formatJSONResponse({
